526668539408336667deebb2486f49ef52ead144
Alan Greasley <alan.greasley@weareadaptive.com>
2022-11-03 11:25:15 +0000

    test SW - LiveRates to 3

--- src/client/src/App/LiveRates/MainHeader/MainHeader.tsx
+++ src/client/src/App/LiveRates/MainHeader/MainHeader.tsx
@@ -59,7 +59,7 @@ export const MainHeader: React.FC = () => {
           </NavItem>
         ))}
       </LeftNav>
-      <LeftNavTitle>Live Alan 2</LeftNavTitle>
+      <LeftNavTitle>Live Alan 3</LeftNavTitle>
       <RightNav>
         <CurrencyDropdown>
           <DropdownMenu
