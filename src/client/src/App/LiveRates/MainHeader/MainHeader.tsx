c8f7fc7377d5d1d2686881e325e40e0933c3fc10
Alan Greasley <alan.greasley@weareadaptive.com>
2022-11-07 18:09:58 +0000

    test SW - LiveRates to 11, no other changes

--- src/client/src/App/LiveRates/MainHeader/MainHeader.tsx
+++ src/client/src/App/LiveRates/MainHeader/MainHeader.tsx
@@ -59,7 +59,7 @@ export const MainHeader: React.FC = () => {
           </NavItem>
         ))}
       </LeftNav>
-      <LeftNavTitle>Live Alan 10</LeftNavTitle>
+      <LeftNavTitle>Live Alan 11</LeftNavTitle>
       <RightNav>
         <CurrencyDropdown>
           <DropdownMenu
