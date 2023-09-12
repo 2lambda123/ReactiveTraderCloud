2683dd56fcde36df86e0f2d97dbbbb01568052d8
Stephan Thibodeau <103059406+stephan-thibodeau@users.noreply.github.com>
2023-09-12 17:21:32 -0400

    update branch yml file for testing purposes

--- packages/client/e2e/global.setup.ts
+++ packages/client/e2e/global.setup.ts
@@ -2,10 +2,11 @@
 import { test as setup } from '@playwright/test'
 import { launch } from 'openfin-adapter'
 
-const E2E_RTC_WEB_ROOT_URL = process.env.E2E_RTC_WEB_ROOT_URL ?? "http://localhost:1917"
+const openfinRootUrl = process.env.E2E_RTC_WEB_ROOT_URL ?? "http://localhost:1917"
 
-setup('Launching application...', async () => {
-  await launch({ manifestUrl: `${E2E_RTC_WEB_ROOT_URL}/config/rt-fx.json`})
-  await launch({ manifestUrl: `${E2E_RTC_WEB_ROOT_URL}/config/rt-credit.json`})
-  await launch({ manifestUrl: `${E2E_RTC_WEB_ROOT_URL}/config/limit-checker.json`})
+setup('Launching application... not', async () => {
+
+  // await launch({ manifestUrl: `${openfinRootUrl}/config/rt-fx.json`})
+  // await launch({ manifestUrl: `${openfinRootUrl}/config/rt-credit.json`})
+  // await launch({ manifestUrl: `${openfinRootUrl}/config/limit-checker.json`})
 })
