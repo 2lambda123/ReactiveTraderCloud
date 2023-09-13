061804d6ce7dc13ffcb462df22bfa9ea3c0c4b0e
Stephan Thibodeau <103059406+stephan-thibodeau@users.noreply.github.com>
2023-09-13 10:00:22 -0400

    use openfin:run:fx

--- packages/client/e2e/global.setup.ts
+++ packages/client/e2e/global.setup.ts
@@ -1,10 +1,11 @@
 // we need to start the runtime and know the devtools port
+import { } from '@openfin/automation-helpers'
 import { test as setup } from '@playwright/test'
 import { launch } from 'openfin-adapter'
 
 const openfinRootUrl = process.env.E2E_RTC_WEB_ROOT_URL ?? "http://localhost:1917"
 
-setup('Launching application... not', async () => {
+setup('Launching application...', async () => {
 
   // await launch({ manifestUrl: `${openfinRootUrl}/config/rt-fx.json`})
   // await launch({ manifestUrl: `${openfinRootUrl}/config/rt-credit.json`})
